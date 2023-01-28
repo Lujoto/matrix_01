dd709f7e12e88c1b906c0f8d59a73542f2f7c9ed
Joto <lujo.townsley@gmail.com>
2023-01-28 10:42:32 -0600

    xor done, moving to chapter 7

--- xor.asm
+++ xor.asm
@@ -11,16 +11,32 @@ section .data
 
 section .bss
 
+rash resb 64
+
 section .text
     global main
 main:
 
 push   rbp
-mov    rbp, rsp
+mov    rbp, rsp ; these two steps happen prior to the breakpoint in main for some reason
+
 
 mov rax, -1     ; fill rax with 1s
+mov [rash], rax
+mov rcx, rash
+
 mov al, byte [bNum]
+mov [rash], rax
+mov rcx, rash 
+
 xor rax, rax
+mov al, byte [bNum]
+
+mov rax, -1
+mov eax, dword [dNum] ;this clears the upper 32 bits of rax, where the bNum does not.
+xor rcx, rcx
+mov [rash], rax
+mov rcx, rash
 
 exit
 
