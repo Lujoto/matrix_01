22373b3127c57db8357c145fe9ea411966d1678b
Joto <lujo.townsley@gmail.com>
2023-02-13 20:50:31 -0600

    don't use casts

--- prime.c
+++ prime.c
@@ -8,13 +8,18 @@
 int main() {
 
 
-int *p = factors(10, K);
+//int *p = factors(10, K);
+size_t t = 4;
+int *p = alloc(t);
 
-for (int i = 0; i < K; i++) {
+/*for (int i = 0; i < K; i++) {
         printf(" %d ", p[i] );
-}
+}*/
 
 free(p);
+
+printf("%cthe size of int is %lu %cthe size of int* is %lu%c", 
+        '\n', sizeof(int), '\n', sizeof(int*), '\n');
 
 /*  for (int i = 0; i < N; i++) {
         if (isPrime(i)) {
--- prime.h
+++ prime.h
@@ -2,7 +2,8 @@
 #define PRIME_
 #include <stdlib.h> 
 #include <stdbool.h>
-#include <math.h> 
+#include <math.h>
+#include <stdio.h>  
 
 // coprime: find all smaller numbers that share no common factors. "Relatively Prime"
 int* coprimes(int a, size_t n) {
@@ -56,21 +57,24 @@ bool isPrime(int a) {
 
 
 int* alloc(size_t a) {
-    int* p = (int*)malloc(sizeof(int)*a);
-    for (int i = 0; i < a; i++) {
+    int* p = malloc(sizeof(int)*a);
+    for (int i = 0; i < a+1; i++) { 
         p[i] = i;
         printf(" %d ", p[i]);
     }
 
+    printf("   size of int* %c %lu %c", '\n', sizeof(int), '\n');
+
+    //p = (int*)realloc(p, a+1);
+    int* j = realloc(p, sizeof(int)*(a+1));
+
+    for (int i = 0; i < a+2; i++) {
+        j[i] = i;
+        printf(" %d ", j[i]);
+    }       
+
     printf("%c", '\n');
 
-
-    p = (int*)realloc(p, a+1);
-   
-    for (int i = 0; i < a; i++) {
-        p[i] = i;
-        printf(" %d ", p[i]);
-    }       
 
     return p;
 }
