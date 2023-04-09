964cf5abd8286a41bf5ff2903cb9071f9558207b
Joto <lujo.townsley@gmail.com>
2023-04-08 22:50:02 -0500

    working angbet too

--- rot.c
+++ rot.c
@@ -13,7 +13,7 @@ int main() {
 //the desired axis of rotation as an argument
 //still need to figure out if this is about a fixed axis or intrinsic
 //rotations. 
-//    double* temp = rot3Dx(rot3Dz(a, PI/3), PI/6);
+    double* temp = rot3Dx(rot3Dz(a, PI/3), PI/6);
     
     for(int i = 0; i < 3; i++) {
         printf("%s\n", "rotation temp");
